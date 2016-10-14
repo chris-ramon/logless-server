98f70c01c40d819b5c206291b69bb0bf64a9c9db
Bela Vizy <bela@xappmedia.com>
2016-10-14 11:20:46 -0400

    keeping lint happy

--- bin/bst-logging-server.ts
+++ bin/bst-logging-server.ts
@@ -252,7 +252,7 @@ app.post("/v1/receive", function (req, res) {
                 return callback(err, l1);
 
             callback(null, l1);
-        })
+        });
     }, function(err, results) {
         if (err) {
             res.json({info: "Error during log entry create", error: err});
@@ -360,5 +360,5 @@ app.get("/v1/log", function (req, res) {
 });
 
 let _server = app.listen(parseInt(serverConfig.server_port), function () {
-    console.log("The BST logger server listening on port "+serverConfig.server_port);
+    console.log("The BST logger server listening on port " + serverConfig.server_port);
 });
--- lib/server-config.ts
+++ lib/server-config.ts
@@ -1,4 +1,3 @@
-import {mongo} from "mongoose";
 /**
  * Created by bvizy on 10/12/16.
  */
@@ -11,14 +10,14 @@ export class ServerConfig {
     public swagger_url: string;
 
     public static create(): ServerConfig {
-        let instance:ServerConfig = new ServerConfig();
+        let instance: ServerConfig = new ServerConfig();
         instance.initialize();
 
         return instance;
     }
 
-    public initialize():void {
-        let props:any = null;
+    public initialize(): void {
+        let props: any = null;
 
         try {
             props = PropertiesReader("./config.properties");
@@ -28,19 +27,19 @@ export class ServerConfig {
 
         let env = process.env.env || "default";
 
-        this.server_port = props.get(env+".server_port" ) || props.get("default.server_port" );
-        this.swagger_url = props.get(env+".swagger_url" ) || props.get("default.swagger_url" );
+        this.server_port = props.get(env + ".server_port") || props.get("default.server_port");
+        this.swagger_url = props.get(env + ".swagger_url") || props.get("default.swagger_url");
 
-        this.mongo_url = process.env.BST_MONGO_URL || props.get(env+".mongo_url" ) || props.get("default.mongo_url" );
+        this.mongo_url = process.env.BST_MONGO_URL || props.get(env + ".mongo_url") || props.get("default.mongo_url");
 
         if (!this.mongo_url) {
             console.error("Mongo db url is not set. Use the BST_MONGO_URL shell environment variable!");
             process.exit(1);
         }
 
-        console.log("Server environment: "+env);
-        console.log("Port: "+this.server_port);
-        console.log("Mongo Url: "+this.mongo_url);
-        console.log("Swagger Url: "+this.swagger_url);
+        console.log("Server environment: " + env);
+        console.log("Port: " + this.server_port);
+        console.log("Mongo Url: " + this.mongo_url);
+        console.log("Swagger Url: " + this.swagger_url);
     }
 }
